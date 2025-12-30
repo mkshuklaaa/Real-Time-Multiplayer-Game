1aa0dca505baca34fea3458e2cd363f4c99fd62c
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2025-12-30 21:51:33 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
-import { io } from "socket.io-client";
-
 const socket = io("https://real-time-multiplayer-game-xzyn.onrender.com");
 
 export default socket;
