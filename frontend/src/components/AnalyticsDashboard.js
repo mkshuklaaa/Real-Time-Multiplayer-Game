9b0f12902619b3c30430625f41b5357af52f7eef
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2026-01-01 19:23:37 +0530

    Update AnalyticsDashboard.js

--- frontend/src/components/AnalyticsDashboard.js
+++ frontend/src/components/AnalyticsDashboard.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const AnalyticsDashboard = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/api/analytics")
+    fetch("https://real-time-multiplayer-game-xzyn.onrender.com/api/analytics")
       .then((res) => res.json())
 
       .then(setData);
@@ -42,3 +42,4 @@ const AnalyticsDashboard = () => {
   );
 };
 export default AnalyticsDashboard;
+
